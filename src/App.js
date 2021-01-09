afd387153962ce984c38b5ffb1a757bd44de4acf
Jappanjeet Singh <jappanjeet.99@gmail.com>
2021-01-09 21:34:50 +0530

    changing somethings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react"
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Layout from "./components/Layout/Layout"
+import Layout from "./containers/Layout/Layout"
 import Homepage from "./containers/Homepage/Homepage"
 import Playground from "./containers/Playground/Playground"
 import ContactUs from "./components/ContactUs/ContactUs"
@@ -10,11 +10,7 @@ import Logout from "./containers/Auth/Logout/Logout"
 import { UseStateValue } from "./store/StateProvider"
 
 const App = () => {
-<<<<<<< HEAD
   const [{ user }] = UseStateValue();
-=======
-  const [{user}] = UseStateValue();
->>>>>>> 5f4f67b28c83370c2dc4db5e0eec308fbc568d02
   let routes;
   if (!user) {
     routes = (
@@ -31,17 +27,11 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/contact-us" component={ContactUs} />
-<<<<<<< HEAD
           <Route exact path="/playground" component={Playground} />
           {/* <Route exact path="/logout" component={Logout} /> */}
           <Redirect to="/playground" />
-=======
-          <Route exact path="/Playground" component={Playground} />
-          <Route exact path="/u/logout" component={Logout} />
-          {/* <Redirect to="/Playground" /> */}
->>>>>>> 5f4f67b28c83370c2dc4db5e0eec308fbc568d02
         </Switch>
-      </Suspense>
+      </Suspense >
     );
   }
 
