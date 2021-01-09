8235dcacd951f8f1a621f89cef4ba95c1ece14c9
Jappanjeet Singh <jappanjeet.99@gmail.com>
2021-01-09 16:09:18 +0530

    updating styles

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -2,6 +2,10 @@ import React, { Component } from "react"
 import Wrapper from "../../hoc/Wrapper"
 import classes from "./Homepage.module.css"
 import Button from "../../components/UI/Button/Button"
+import videoSvg from "../../assets/video-illustration.svg"
+import codeSvg from "../../assets/code-illustration.svg"
+import musicSvg from "../../assets/music-illustration.svg"
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 class Homepage extends Component {
     state = {
@@ -15,39 +19,41 @@ class Homepage extends Component {
             <Wrapper>
                 <div className={classes.Hero}>
                     <div className={classes.Content}>
-                        <h1>Convert Gestures to Music</h1>
+                        <h1>Convert Gestures to <span className={classes.AnimateMusic}>Music</span></h1>
                         <div className={classes.Button}>
                             <Button color="secondary" variant="outlined" clicked={this.continueApplyHandler}>Get Started</Button>
                         </div>
                     </div>
                 </div>
-                {/* <section className="site-section-1">
+                <section className="site-section-1">
                     <div className={classes.CompanyFeaturesSection}>
                         <h1>
                             How it Works?
                         </h1>
-                        <p className={classes.Text}>With just few clicks, students can send their applications to academic programs at multiple institutions. <span>No need to apply by standing for hours in a queue.</span></p>
                         <div className={classes.Features}>
                             <div className={classes.CompanyFeature}>
-                                <AccountCircleIcon className={classes.Icon} />
-                                <h3>1:PROFILE</h3>
-                                <p>Complete sign up process to make your Find My School Account.</p>
+                                <div className={classes.FeatureContent}>
+                                    <img src={videoSvg} alt="illustration" />
+                                    <p>Model trains on input.</p>
+                                </div>
                             </div>
-                            <RedoIcon style={{ "color": "#ffe6e6", "fontSize": "5.5rem", "marginBottom": "100px" }} className={classes.RedoIcon} />
+                            <div id={classes.icon}>→</div>
                             <div className={classes.CompanyFeature}>
-                                <PostAddIcon className={classes.Icon} />
-                                <h3>2:FILL APPLICATION</h3>
-                                <p>Fill the application form with all the details. This will make a generic profile of you which you can use to apply to multiple institutions</p>
+                                <div className={classes.FeatureContent}>
+                                    <img src={codeSvg} alt="illustration" />
+                                    <p>Sonic Pi code gets generated through the video</p>
+                                </div>
                             </div>
-                            <RedoIcon style={{ "color": "#ffe6e6", "fontSize": "5.5rem", "marginBottom": "100px" }} className={classes.RedoIcon} />
+                            <div id={classes.icon}>→</div>
                             <div className={classes.CompanyFeature}>
-                                <SchoolIcon className={classes.Icon} />
-                                <h3>3:PREDICT</h3>
-                                <p>Our Machine Learning Model will predict the best college for you.</p>
+                                <div className={classes.FeatureContent}>
+                                    <img src={musicSvg} alt="illustration" />
+                                    <p>Music is produced</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </section> */}
+                </section>
 
             </Wrapper>
         )
