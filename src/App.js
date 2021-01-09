f4f5a0298cf3c1112b7778e47199e0be0ee573dd
Ritesh Bucha <bucharitesh@gmail.com>
2021-01-09 21:21:03 +0530

    :alien:  Login and Logo

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import Homepage from "./containers/Homepage/Homepage"
 import Playground from "./containers/Playground/Playground"
 import ContactUs from "./components/ContactUs/ContactUs"
 import Login from "./containers/Auth/Login/Login"
-import Register from "./containers/Auth/Register/Register"
+import Logout from "./containers/Auth/Logout/Logout"
 
 import { UseStateValue } from "./store/StateProvider"
 
 const App = () => {
   const [{user}] = UseStateValue();
-
   let routes;
-
   if (!user) {
     routes = (
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/contact-us" component={ContactUs} />
         <Route exact path="/u/login" component={Login} />
-        <Route exact path="/u/register" component={Register} />
         <Redirect to="/u/login" />
       </Switch>
     );
@@ -31,8 +28,8 @@ const App = () => {
           <Route exact path="/" component={Homepage} />
           <Route exact path="/contact-us" component={ContactUs} />
           <Route exact path="/Playground" component={Playground} />
-          {/* <Route exact path="/logout" component={Logout} /> */}
-          <Redirect to="/Playground" />
+          <Route exact path="/u/logout" component={Logout} />
+          {/* <Redirect to="/Playground" /> */}
         </Switch>
     </Suspense>
     );
